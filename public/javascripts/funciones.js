57614685250e1d5f433ffdbb66d8c0b2eb7012c3
Micro <microa4@gmail.com>
2019-04-02 21:06:09 +0200

    added menu in dom

---
+++ public/javascripts/funciones.js
@@ -0,0 +1,12 @@
+function ocultar() {
+    document.getElementById('sel').style.display = 'none';
+    document.getElementById('boton1').style.display = 'block';
+    document.getElementById('boton2').style.display = 'none';
+}
+
+function mostrar() {
+    document.getElementById('boton1').style.display = 'none';
+    document.getElementById('sel').style.display = 'block';
+    document.getElementById('boton2').style.display = 'block';
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+
+/*localhost:3000/    */
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.render('menu');
+  //  res.render('footer');
+  
+  
 });
 
+
+// const hbsUtils = require ('hbs-utils')(hbs);
+// hbs.registerPartials('/partials');
+// hbsUtils.registerWatchedPartials('/partials')
+
+
+// const hbsUtils = require ('hbs-utils')(hbs);
+// hbs.registerPartials(`/views/partials`);
+// hbsUtils.registerWatchedPartials(`/views/partials`)
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+  
