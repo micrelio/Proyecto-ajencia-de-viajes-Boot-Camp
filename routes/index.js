2c11493bee60dab049a6323a57ea0c0cd4f80a8b
Micro <microa4@gmail.com>
2019-04-06 16:24:35 +0200

    mongo configurated

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,50 @@
 var express = require('express');
+const User = require( '../models/user' );
 var router = express.Router();
 
 
 /*localhost:3000/    */
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('uuuuuuuuuuuuuuuuu');
+  // console.log('uuuuuuuuuuuuuuuuu');
   
   res.render('index', { title: 'Express' });
 
 });
 
+
+router.get( '/registro', function ( req, res, next ) {
+  res.render('registro');
+ } );
+
+ router.post( '/registro', function ( req, res, next ) {
+
+  console.log( req.body );
+  new User( req.body )
+      .save()
+      .then( () => {
+          console.log( 'registro valido' );
+
+          email.transporter.sendMail( {
+              to: req.body.email,
+              subject: 'Registro correcto',
+              html: 'Welcome!'
+          }, ( error, info ) => {
+              console.log(error, info);
+          } );
+
+
+          res.render( 'register', { message: 'Registro válido. Ya puedes hacer login' } );
+
+
+      } )
+      .catch( ( err ) => {
+          console.log( 'registro invalido', err );
+
+          res.render( 'registro', { error: err.message } );
+
+      } )
+} );
 
 
 router.get( '/login', function ( req, res, next ) {
@@ -23,22 +57,27 @@ router.get( '/login', function ( req, res, next ) {
 } );
 
 
-router.get( '/registro', function ( req, res, next ) {
- res.render('registro');
+router.post( '/login', function ( req, res, next ) {
+
+  console.log( req.body );
+
+  User.findOne( req.body )
+      .then( ( user ) => {
+          console.log( 'login valido', user );
+          if ( user ) {
+              res.render( 'login', { message: 'Bienvenido al castillo! ' + user.email } );
+          } else {
+              res.render( 'login', { error: 'credenciales incorrectos' } );
+
+          }
+      } )
+      .catch( ( err ) => {
+          console.log( 'login invalido', err );
+          res.render( 'login', { error: 'Ups algo no ha ido bien.  vuelva intentarlo más tarde' } );
+
+      } )
 } );
 
-
-
-
-
-
-
-
-// router.post( '/menu', function ( req, res, next ) {
-//   console.log( 'AAAAAAAAAAAAAAA' );
-//   console.log( req.body );
-  
-//           } );
 
 module.exports = router;
 
